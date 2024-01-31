981811b6f6b10447df1e282d1cd32db55e707ef5
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2024-01-31 13:26:48 +0700

    2. datatypes

--- TypescriptDemo/datatypes.js
+++ TypescriptDemo/datatypes.js
@@ -1,2 +1,27 @@
 "use strict";
-let lname = 5;
+let lname;
+lname = "Mujahid";
+let newName = lname.toUpperCase();
+console.log(newName);
+let age;
+age = 25;
+age = 25.5;
+let ageInString = "25";
+let newAge = parseInt(ageInString);
+let isMarried;
+// console.log(isMarried) compile error, isMarried is not defined
+isMarried = false;
+console.log(isMarried);
+let emp;
+emp = ["Mujahid", "Ansori", "Majid"];
+let numbers = [1, 2, 3, 4, 5, 6];
+let moreThanThree = numbers.filter(num => num > 3);
+let sum = numbers.reduce((a, b) => a + b, 0);
+console.log(moreThanThree, sum);
+let blue = "BLUE" /* Color.BLUE */;
+console.log(blue);
+// tupples
+let swapNumbers;
+const swap = (a, b) => [b, a];
+swapNumbers = swap(1, 2);
+console.log(swapNumbers);
--- TypescriptDemo/datatypes.ts
+++ TypescriptDemo/datatypes.ts
@@ -1,4 +1,52 @@
 let lname: string;
 
 lname = "Mujahid";
-lname.toUpperCase();
+let newName = lname.toUpperCase();
+console.log(newName);
+
+let age: number;
+age = 25;
+age = 25.5;
+
+let ageInString: string = "25";
+let newAge = parseInt(ageInString);
+
+let isMarried: boolean;
+
+// console.log(isMarried) compile error, isMarried is not defined
+
+isMarried = false;
+
+console.log(isMarried);
+
+
+let emp: string[];
+emp = ["Mujahid", "Ansori", "Majid"];
+
+let numbers: Array<number> = [1, 2, 3, 4, 5, 6];
+let moreThanThree = numbers.filter(num => num > 3);
+let sum = numbers.reduce((a, b) => a + b, 0);
+
+console.log(moreThanThree, sum);
+
+
+// enum
+const enum Color {
+    RED = "RED",
+    GREEN = "GREEN",
+    BLUE = "BLUE",
+    YELLOW = "YELLOW"
+}
+
+let blue: Color = Color.BLUE;
+console.log(blue);
+
+// tuples
+
+let swapNumbers: [number, number];
+const swap = (a: number, b: number): [number, number] => [b, a];
+
+swapNumbers = swap(1, 2);
+console.log(swapNumbers)
+
+
