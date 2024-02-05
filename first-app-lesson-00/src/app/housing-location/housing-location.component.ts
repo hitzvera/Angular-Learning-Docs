b3bc33b376f309ddd9689729efddb920c72835d5
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2024-02-05 11:15:58 +0700

    implementing router in angular

--- first-app-lesson-00/src/app/housing-location/housing-location.component.ts
+++ first-app-lesson-00/src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   template: `
     <section class="listing">
       <img
@@ -17,6 +18,7 @@ import { HousingLocation } from '../housinglocation';
       <p class="listing-location">
         {{ housingLocation.city }}, {{ housingLocation.state }}
       </p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styleUrl: './housing-location.component.css',
