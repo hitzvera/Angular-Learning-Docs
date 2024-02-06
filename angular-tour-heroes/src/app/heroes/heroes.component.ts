802c886b4d4372bd7baa2db405166ba3aee50eb2
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2024-02-06 23:21:27 +0700

    adding detail hero with different component

--- angular-tour-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-heroes/src/app/heroes/heroes.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-
+import { UpperCasePipe, NgFor, NgIf } from '@angular/common';
+import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [NgFor, HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
