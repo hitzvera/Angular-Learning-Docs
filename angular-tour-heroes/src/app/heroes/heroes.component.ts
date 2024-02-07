b86e15c397893741cf26bef7d235cd542e53327a
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2024-02-07 16:52:50 +0700

    implementing Fetching data asyncly with Observable

--- angular-tour-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-heroes/src/app/heroes/heroes.component.ts
@@ -3,6 +3,7 @@ import { Hero } from '../hero';
 import { UpperCasePipe, NgFor, NgIf } from '@angular/common';
 import { HEROES } from '../mock-heroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   standalone: true,
@@ -11,9 +12,18 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   styleUrl: './heroes.component.css',
 })
 export class HeroesComponent {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
 
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
+  }
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
